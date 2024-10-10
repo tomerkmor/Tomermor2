6a6feb1d2ba9af967768bcb11a7ef59eedc4f17f
tomerkmor <sexylooter@gmail.com>
2024-10-10 08:35:38 +0300

    ready for deployment

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const connectToDB = async () => {
   if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(process.env.MONGODB_URI!, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI!);
   }
 };
 
