e93b3b3e5f9a06ec9949543d86b9285183d10fcd
tomerkmor <sexylooter@gmail.com>
2024-10-24 01:41:14 +0300

    ready for deployment

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,7 +1,7 @@
-import connectToDB from '@/lib/db'; 
-import bcrypt from 'bcrypt';
-import User from '@/models/User'; 
-import jwt from 'jsonwebtoken'; // Use JWT for token generation
+import connectToDB from "@/lib/db";
+import bcrypt from "bcrypt";
+import User from "@/models/User";
+import jwt from "jsonwebtoken"; // Use JWT for token generation
 
 export async function POST(req) {
   await connectToDB();
@@ -9,26 +9,40 @@ export async function POST(req) {
   try {
     const { username, password } = await req.json();
 
-    console.log(username, password)
+    console.log(username, password);
     // Check if the user exists
     const user = await User.findOne({ username });
     if (!user) {
-      return new Response(JSON.stringify({ error: 'User not found' }), { status: 404 });
+      return new Response(JSON.stringify({ error: "User not found" }), {
+        status: 404,
+      });
     }
 
     // Compare the password with the stored hash
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
     if (!isPasswordCorrect) {
-      return new Response(JSON.stringify({ error: 'Invalid credentials' }), { status: 401 });
+      return new Response(JSON.stringify({ error: "Invalid credentials" }), {
+        status: 401,
+      });
     }
 
     // Generate a JWT token for authentication
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    if (!process.env.JWT_SECRET) {
+      throw new Error("Missing JWT_SECRET environment variable");
+    }
 
-    return new Response(JSON.stringify({ message: 'Login successful', token }), { status: 200 });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return new Response(
+      JSON.stringify({ message: "Login successful", token }),
+      { status: 200 }
+    );
   } catch (error) {
-    return new Response(JSON.stringify({ error: 'An error occurred' }), { status: 500 });
+    console.error("Login Error:", error); // Add this for better error debugging
+    return new Response(JSON.stringify({ error: "An error occurred" }), {
+      status: 500,
+    });
   }
 }
-
-
