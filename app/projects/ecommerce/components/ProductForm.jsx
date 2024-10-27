bc9fdec056fd43f4e88709c33f2939d0ddc34deb
tomerkmor <sexylooter@gmail.com>
2024-10-27 16:28:47 +0200

    ready for deployment

--- app/projects/ecommerce/components/ProductForm.jsx
+++ app/projects/ecommerce/components/ProductForm.jsx
@@ -17,14 +17,14 @@ const ProductForm = ({
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [imagePreviews, setImagePreviews] = useState(existingImages || []);
   const [imagesAreEdited, setImagesAreEdited] = useState(false);
-  
+
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
 
   const router = useRouter();
 
   const handleUpdatePhoto = (e) => {
-    setImagesAreEdited(true)
+    setImagesAreEdited(true);
     const files = Array.from(e.target.files);
     setSelectedFiles(files);
 
@@ -130,6 +130,21 @@ const ProductForm = ({
               accept="image/*"
               multiple
             />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5 text-gray-500"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+              />
+            </svg>
+
             <span className="text-gray-500">Upload</span>
           </label>
 
